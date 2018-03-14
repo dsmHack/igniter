8d61ea320a63b8ff569078946d0e80dbcef17061
Joshua <inc@kickercost.com>
2018-03-14 00:36:07 -0500

    seperating config from registry

---
+++ src/main/java/com/dsmhack/igniter/services/IntegrationServicesRegistry.java
@@ -0,0 +1,34 @@
+package com.dsmhack.igniter.services;
+
+import com.dsmhack.igniter.configuration.IntegrationServicesConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class IntegrationServicesRegistry {
+
+    private IntegrationServicesConfiguration integrationServicesConfiguration;
+
+    private final List<IntegrationService> availableServices;
+
+    @Autowired
+    public IntegrationServicesRegistry(IntegrationServicesConfiguration integrationServicesConfiguration, List<IntegrationService> availableServices) {
+        this.integrationServicesConfiguration = integrationServicesConfiguration;
+        this.availableServices = availableServices;
+    }
+
+    public List<IntegrationService> getActiveIntegrationServices() {
+        return this.availableServices.stream()
+                .filter(integrationService -> integrationServicesConfiguration.getActiveIntegrations().contains(integrationService.getIntegrationServiceName()))
+                .collect(Collectors.toList());
+    }
+
+    public void activateIntegrationService(String integrationServiceName) {
+        if (!integrationServicesConfiguration.getActiveIntegrations().contains(integrationServiceName)) {
+            integrationServicesConfiguration.getActiveIntegrations().add(integrationServiceName);
+        }
+    }
+}
