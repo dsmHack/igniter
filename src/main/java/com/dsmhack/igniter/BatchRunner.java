70999beda06c8ece9fb11c3ca66560f6fced1c67
Chuck Rolek <c.rolek@gmail.com>
2018-03-18 22:11:25 -0500

    WIP Autowire silliness

--- src/main/java/com/dsmhack/igniter/BatchRunner.java
+++ src/main/java/com/dsmhack/igniter/BatchRunner.java
@@ -1,29 +1,28 @@
 package com.dsmhack.igniter;
 
-import com.dsmhack.igniter.models.User;
+import com.dsmhack.igniter.services.TeamConfigurationService;
 import com.dsmhack.igniter.services.UserImportService;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Getter
 @Setter
+@Service
 public class BatchRunner {
     private UserImportService userImportService;
+    private TeamConfigurationService teamConfigurationService;
 
     public BatchRunner() {}
 
     @Autowired
-    public BatchRunner(UserImportService userImportService) {
+    public BatchRunner(UserImportService userImportService, TeamConfigurationService teamConfigurationService) {
         this.userImportService = userImportService;
+        this.teamConfigurationService = teamConfigurationService;
     }
 
-
-    String team = "whatever~";
-
-    public void onboardSlack(){
-        System.out.println("team: " + team);
-        User user = this.userImportService.parseStringIntoUser("idk");
+    public void onboardEveryone() {
 
     }
 
