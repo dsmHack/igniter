40951a6aa061c00087049036bb52a8c3ffbb65fd
Chuck Rolek <c.rolek@gmail.com>
2018-03-20 21:11:12 -0500

    TestBatchRunner with autowire works

--- src/main/java/com/dsmhack/igniter/BatchRunner.java
+++ src/main/java/com/dsmhack/igniter/BatchRunner.java
@@ -1,5 +1,7 @@
 package com.dsmhack.igniter;
 
+import com.dsmhack.igniter.configuration.IntegrationServicesConfiguration;
+import com.dsmhack.igniter.models.User;
 import com.dsmhack.igniter.services.TeamConfigurationService;
 import com.dsmhack.igniter.services.UserImportService;
 import lombok.Getter;
@@ -7,21 +9,31 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Service
 public class BatchRunner {
+
     private final UserImportService userImportService;
+    private final IntegrationServicesConfiguration integrationServicesConfiguration;
     private final TeamConfigurationService teamConfigurationService;
 
     @Autowired
-    public BatchRunner(TeamConfigurationService teamConfigurationService,UserImportService userImportService) {
+    public BatchRunner(TeamConfigurationService teamConfigurationService, UserImportService userImportService, IntegrationServicesConfiguration integrationServicesConfiguration) {
         this.teamConfigurationService = teamConfigurationService;
         this.userImportService = userImportService;
+        this.integrationServicesConfiguration = integrationServicesConfiguration;
     }
 
-    public void onboardEveryone() {
-
+    public void onboardEveryone(String fileName) {
+        System.out.println("onboard.filename: " + fileName);
+//        List<User> usersByList = userImportService.getUsersByList(fileName);
+//        for (Integer i = 0; i < integrationServicesConfiguration.getTeamNumber(); i++) {
+//            String compositeName = integrationServicesConfiguration.getCompositeName( i.toString());
+//            teamConfigurationService.createTeam(compositeName);
+//            usersByList.forEach(user ->{teamConfigurationService.addUserToTeam(compositeName,user);});
+//        }
     }
-
 }
--- src/test/java/com/dsmhack/igniter/TestBatchRunner.java
+++ src/test/java/com/dsmhack/igniter/TestBatchRunner.java
@@ -1,41 +1,21 @@
 package com.dsmhack.igniter;
 
-import com.dsmhack.igniter.models.User;
-import com.dsmhack.igniter.services.TeamConfigurationService;
-import com.dsmhack.igniter.services.UserImportService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootContextLoader;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.mockito.Mockito.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = IgniterApplication.class, loader = SpringBootContextLoader.class )
-public class TestBatchRunner {
-    @Autowired
-    private UserImportService userImportService;
-    @Autowired
-    private TeamConfigurationService teamConfigurationService;
+public class TestBatchRunner extends SpringContextConfiguration {
     @Autowired
     BatchRunner batchRunner;
 
     @Test
     public void testOnboardSlack_createsStuff(){
-//        UserImportService fakeUserImportService = mock(UserImportService.class);
-        User aUser = new User("a", "b", "c", "d");
-//        when(fakeUserImportService.parseStringIntoUser(anyString())).thenReturn(aUser);
-//        BatchRunner batchRunner = new BatchRunner(fakeUserImportService, teamConfigurationService);
-//        if(this.teamConfigurationService != null) {
-//        }
-        //aseertEquals("idk", batchRunner, instanceOf(BatchRunner.class));
+
         System.out.println("runner: " + batchRunner);
+
+        batchRunner.onboardEveryone("bleh");
 
     }
 }
